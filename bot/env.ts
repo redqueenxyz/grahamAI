9821c96233256d50235a009e684bebe75fc94d43
Vivek Menon <mail@vvkmnn.xyz>
2017-10-09 23:28:26 -0400

    (fix): Better `.env`

--- bot/env.ts
+++ bot/env.ts
@@ -1,12 +1,7 @@
 // Package Dependencies
 import * as dotenv from 'dotenv'
-import * as admin from 'firebase-admin'
-import * as functions from 'firebase-functions'
 
 /// Config
 
 // Process Environment
 dotenv.config()
-
-// Firebase
-admin.initializeApp(functions.config().firebase)
