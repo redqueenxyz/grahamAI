ac5d6dbbf19c2ea10ff0ff84d513df5e1147bb91
Vivek Menon <mail@vvkmnn.xyz>
2017-10-30 15:00:33 -0400

    FIX(project): Different entry point for testing.

--- bot/project.clj
+++ bot/project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[krak "0.1.1"]
                  [org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot bot.core
+  :main ^:skip-aot bot.buyer
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
