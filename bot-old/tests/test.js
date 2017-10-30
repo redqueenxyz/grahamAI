3f6b2e808ffbe10fb92d061839df6b9df0d27d9b
Vivek Menon <mail@vvkmnn.xyz>
2017-10-30 11:50:27 -0400

    BREAK(bot): Moving the logic into the new ClojureScript

--- bot-old/tests/test.js
+++
@@ -1,3 +0,0 @@
-let expect = require('chai').expect;
-// https://zaiste.net/posts/modern_node_js_async_await_based_testing_with_mocha_chai/
-
--- bot/logic/test.clj
+++
@@ -1,5 +0,0 @@
-(ns clojurescript-example-project.core)
-
-(enable-console-print!)
-
-(.log js/console "Hello world!")
